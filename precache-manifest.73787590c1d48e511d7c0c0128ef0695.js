25f6e44cc46fb35859e7b074b93fd0a3bfc7c860
WBelchman <william.belcher@sps.edu>
2020-07-21 14:01:07 -0600

    Updates

---
+++ precache-manifest.73787590c1d48e511d7c0c0128ef0695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0844b23e4baf0b050d13edc2c3f74e6d",
+    "url": "/funnyquotes/index.html"
+  },
+  {
+    "revision": "407df0b36c15588a4fbb",
+    "url": "/funnyquotes/static/css/main.e8419ee6.chunk.css"
+  },
+  {
+    "revision": "07c5fc0fc769bf175bcc",
+    "url": "/funnyquotes/static/js/2.4cb63c53.chunk.js"
+  },
+  {
+    "revision": "7e6a6e49d7e725504aff8f50cc381878",
+    "url": "/funnyquotes/static/js/2.4cb63c53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407df0b36c15588a4fbb",
+    "url": "/funnyquotes/static/js/main.2b72f78a.chunk.js"
+  },
+  {
+    "revision": "b6ea1cc3a7698c29cf38",
+    "url": "/funnyquotes/static/js/runtime-main.ec209800.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/funnyquotes/precache-manifest.745488c52a37989499b1e17d99df4ee2.js"
+  "/funnyquotes/precache-manifest.73787590c1d48e511d7c0c0128ef0695.js"
 );
 
 self.addEventListener('message', (event) => {
